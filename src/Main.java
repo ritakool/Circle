e5a4407a580dc9e9397a7713dde265b4e7964c92
ritakool <jokernguyen1994@gmail.com>
2023-03-28 17:17:19 +0700

    done

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new Circle(5.0);
+        System.out.println("Radius = " + c.getRadius());
+        System.out.println("Area = " + c.getArea());
+    }
+}
